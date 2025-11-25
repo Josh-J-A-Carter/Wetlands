aa1df68e179adc3b7c07927e348f66db2f251b68
Josh <josh.ja.carter@gmail.com>
2025-11-26 12:46:47 +1300

    Base of tree growth algorithm. Missing side branches, cladoptosis, and light calculations

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Tree : MonoBehaviour {
 
+    public const float GROWTH_TICK_DELAY = 0.005f;
+    public const float GROWTH_TICK_INCR = 0.0001f;
+
+    [SerializeField]
     TreeParameters currentParameters;
     
     public TreeBranch trunk { get; private set; }
@@ -15,34 +20,29 @@ public class Tree : MonoBehaviour {
 
     public void Start() {
         trunk = new();
-        trunk.TestAddNodes(new() {
-            new(Vector3.zero, 2.0f),
-            new(new(0, 2, 0), 1.5f),
-        //     new(new(0, 4, 0), 1.0f),
-        //     new(new(0, 6, 0), 0.5f)
-        // }, new(new(0, 8, 0), 0.25f), new(0, 1.0f, 0));
-        }, new(new(0, 6, 0), 0.5f), new(0, 1.0f, 0));
+        // trunk.TestAddNodes(new() {
+        //     new(Vector3.zero, 2.0f),
+        //     new(new(0, 2, 0), 1.5f),
+        // //     new(new(0, 4, 0), 1.0f),
+        // //     new(new(0, 6, 0), 0.5f)
+        // // }, new(new(0, 8, 0), 0.25f), new(0, 1.0f, 0));
+        // }, new(new(0, 6, 0), 0.5f), new(0, 1.0f, 0));
 
 
-        TreeBranch sidebranch = new();
-        sidebranch.TestAddNodes(new() {
-            new(new(-1.3f, 2.5f, 1.3f), 0.25f),
-            new(new(-1.85f, 4.0f, 1.85f), 0.2f),
-            new(new(-2.25f, 5.5f, 2.25f), 0.15f)
-        }, new(new(-2.75f, 7.0f, 2.75f), 0.1f), new(-1.0f, 3.0f, 1.0f));
+        // TreeBranch sidebranch = new();
+        // sidebranch.TestAddNodes(new() {
+        //     new(new(-1.3f, 2.5f, 1.3f), 0.25f),
+        //     new(new(-1.85f, 4.0f, 1.85f), 0.2f),
+        //     new(new(-2.25f, 5.5f, 2.25f), 0.15f)
+        // }, new(new(-2.75f, 7.0f, 2.75f), 0.1f), new(-1.0f, 3.0f, 1.0f));
 
-        trunk.TestAddBranch(sidebranch, 1);
+        // trunk.TestAddBranch(sidebranch, 1);
         
         // Make the mesh
         mesh = new();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        (List<Vector3> vertices, List<int> triangles, List<GizmoData> gizmos) = TreeMeshGenerator.Generate(this, 4);
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        this.gizmos = gizmos;
-
-        mesh.RecalculateNormals();
+        StartCoroutine(GrowthTick());
     }
 
 
@@ -63,9 +63,43 @@ public class Tree : MonoBehaviour {
     }
 
 
+    IEnumerator GrowthTick() {
+        while (true) {
+            // Calculate light absorbed
+            float light = 1.0f;
+
+            // Growth
+            trunk.Grow(light, Vector3.zero, currentParameters);
+
+            // Recursively add new side branches
+
+            // Update the mesh
+            (List<Vector3> vertices, List<int> triangles, List<GizmoData> gizmos) = TreeMeshGenerator.Generate(this, 4);
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            this.gizmos = gizmos;
+
+            mesh.RecalculateNormals();
+
+            yield return new WaitForSeconds(GROWTH_TICK_DELAY);
+        }
+    }
+
+
 }
 
 
+[Serializable]
 public class TreeParameters {
-    public float apicalDominance { get; private set; }
+    [SerializeField]
+    public float apicalDominance = 0.5f;
+
+    [SerializeField]
+    public float growthSpeed = 1.0f;
+
+    [SerializeField]
+    public float widthToLenGrowthRatio = 0.05f;
+
+    [SerializeField]
+    public float internodeLength = 0.5f;
 }
--- Assets/Scripts/TreeBranch.cs
+++ Assets/Scripts/TreeBranch.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -13,11 +14,16 @@ public class TreeBranch {
     List<TreeNode> nodes;
 
     public List<Tuple<int, TreeBranch>> sideBranches { get; private set; }
-
+    public List<Tuple<int, Vector3>> inactiveBuds { get; private set; }
 
     public TreeBranch() {
         nodes = new();
         sideBranches = new();
+        inactiveBuds = new();
+
+        nodes.Add(new(Vector3.zero, 0.001f));
+        terminus = new(new(0, 0.001f, 0), 0.001f);
+        terminusDirection = Vector3.up;
     }
 
     public void TestAddNodes(List<TreeNode> nodes, TreeNode terminus, Vector3 dir) {
@@ -38,9 +44,9 @@ public class TreeBranch {
         if (index == NodeCount() - 1) return new(terminus, terminusDirection);
 
         int nextIndex = index + 1;
-        TreeNode nextNode = nextIndex == NodeCount() - 1 ? terminus : nodes[nextIndex];
-
-        return new(nodes[index], nextNode.position - nodes[index].position);
+        Vector3 dir = nextIndex == NodeCount() - 1 ? terminusDirection : (nodes[nextIndex].position - nodes[index].position);
+        
+        return new(nodes[index], dir.normalized);
     }
 
     // Number of nodes in the branch, including the terminal bud.
@@ -59,6 +65,60 @@ public class TreeBranch {
         return branches;
     }
 
+    public List<TreeBranch> GetAllSideBranches() {
+        return new(sideBranches.Select(tuple => tuple.Item2));
+    }
+
+    public void Grow(float light, Vector3 deltaPos, TreeParameters param) {
+        float growthVal = GrowthFactor(param) * light;
+        float lengthGrowthFactor = growthVal * Tree.GROWTH_TICK_INCR;
+        float widthGrowthFactor = param.widthToLenGrowthRatio * growthVal * Tree.GROWTH_TICK_INCR;
+
+        // Apply deltaPos to each (non-terminal) node & enlarge as needed
+        for (int i = 0 ; i < nodes.Count ; i += 1) {
+            nodes[i].Translate(deltaPos);
+            nodes[i].Enlarge(widthGrowthFactor);
+        }
+
+        // Recurse on child branches
+        foreach ((int offshootIndex, TreeBranch branch) in sideBranches) {
+            Vector3 offshootDir = (branch.nodes[0].position - nodes[offshootIndex].position).normalized;
+            Vector3 deltaPosPrime = deltaPos + widthGrowthFactor * offshootDir;
+            branch.Grow(light, deltaPosPrime, param);
+        }
+
+        // Update terminal bud
+        terminus.Enlarge(widthGrowthFactor);
+        terminus.Translate(deltaPos + terminusDirection * lengthGrowthFactor);
+
+        float internodeLength = (terminus.position - nodes.Last().position).magnitude;
+
+        if (internodeLength >= param.internodeLength) {
+            TreeNode newTerminus = new(terminus.position, 0.0f);
+            Vector3 newTerminusDirection = terminusDirection.normalized;
+
+            nodes.Add(terminus);
+
+            terminus = newTerminus;
+            terminusDirection = newTerminusDirection;
+
+            // 
+            //  Create new buds
+            // 
+
+            // 
+            //  Destroy old buds
+            // 
+        }
+    }
+
+    private float GrowthFactor(TreeParameters param) {
+        float s = param.growthSpeed;
+        float d = depth;
+        float a = param.apicalDominance;
+        return s * (Mathf.Exp(-a * d) / (2 - a)  + 1);
+    }
+
 }
 
 
@@ -69,5 +129,13 @@ public class TreeNode {
     public TreeNode(Vector3 position, float width) {
         this.position = position;
         this.width = width;
+    }
+
+    public void Translate(Vector3 deltaPos) {
+        position += deltaPos;
+    }
+
+    public void Enlarge(float deltaWidth) {
+        width += deltaWidth;
     }
 }
